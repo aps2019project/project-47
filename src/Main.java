3448e8d33772e6dbf798f3be055b7e69bd08dac9
mhgheisarieh <44923177+mhgheisarieh@users.noreply.github.com>
2019-06-10 11:32:41 +0430

    add run console

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,11 @@
-import controllers.console.AccountMenu;
-import controllers.console.MainMenu;
-import defentions.Defentions;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
 import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import models.Account;
-import models.cards.hero.Hero;
-import models.cards.minion.Minion;
-import models.cards.spell.Spell;
-import models.deck.Deck;
-
-import java.util.ArrayList;
 
 public class Main extends Application {
 
@@ -31,7 +19,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("layouts/mainPageLayout.fxml"));
-        root.getStylesheets().add("resources/stylesheets/mainPageStyle.css");
+        root.getStylesheets().add("resources/stylesheets/styles.css");
         root.getStyleClass().add("mainPage");
         Image img = new Image(getClass().getResourceAsStream("resources/cursor.png"));
         ImageCursor cursor = new ImageCursor(img, 30, 30);
@@ -45,28 +33,6 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-//        runConsole();
         launch(args);
-    }
-
-    private static void runConsole() {
-        MainMenu mainMenu = new MainMenu();
-        Account Mmd = new Account("Mmd");
-        Deck deck1 = new Deck("best");
-        ArrayList<Minion> minions = Defentions.defineMinion();
-        ArrayList<Hero> heroes = Defentions.defineHero();
-        ArrayList<Spell> spells = Defentions.defineSpell();
-        for (int i = 0; i < 15; i++) {
-            deck1.addCard(minions.get(i));
-        }
-        for (int i = 0; i < 4; i++) {
-            deck1.addCard(spells.get(i));
-        }
-        deck1.addCard(heroes.get(0));
-        Mmd.addDeck(deck1);
-        Mmd.setMainDeck(deck1);
-        AccountMenu.addAccount(Mmd);
-        AccountMenu.setLoginAccount(Mmd);
-        mainMenu.openMenu();
     }
 }
---
+++ src/runConsole.java
@@ -0,0 +1,34 @@
+import controllers.console.AccountMenu;
+import controllers.console.MainMenu;
+import defentions.Defentions;
+import models.Account;
+import models.cards.hero.Hero;
+import models.cards.minion.Minion;
+import models.cards.spell.Spell;
+import models.deck.Deck;
+
+import java.util.ArrayList;
+
+public class runConsole {
+
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        Account Mmd = new Account("Mmd");
+        Deck deck1 = new Deck("best");
+        ArrayList<Minion> minions = Defentions.defineMinion();
+        ArrayList<Hero> heroes = Defentions.defineHero();
+        ArrayList<Spell> spells = Defentions.defineSpell();
+        for (int i = 0; i < 15; i++) {
+            deck1.addCard(minions.get(i));
+        }
+        for (int i = 0; i < 4; i++) {
+            deck1.addCard(spells.get(i));
+        }
+        deck1.addCard(heroes.get(0));
+        Mmd.addDeck(deck1);
+        Mmd.setMainDeck(deck1);
+        AccountMenu.addAccount(Mmd);
+        AccountMenu.setLoginAccount(Mmd);
+        mainMenu.openMenu();
+    }
+}
