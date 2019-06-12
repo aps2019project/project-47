4476f91f5cd4b1f3d1e2c8cce1f70666808555bf
mhgheisarieh <44923177+mhgheisarieh@users.noreply.github.com>
2019-06-12 23:47:25 +0430

    some effects added

--- src/controllers/console/Constants.java
+++ src/controllers/console/Constants.java
@@ -1,10 +1,5 @@
 package controllers.console;
 
-public class Constants {
-    final static int EXIT = 12345;
-    public final static int ACCOUNT_EXISTS = 1;
-    public final static int ACCOUNT_CREATE_SUCCESSFULLY = 2;
-    public final static int INVALID_USERNAME = 3;
-    public final static int SUCCESSFUL_LOGIN = 4;
-    public final static int WRONG_PASSWORD = 5;
+public enum Constants {
+    EXIT , ACCOUNT_EXISTS , ACCOUNT_CREATE_SUCCESSFULLY, INVALID_USERNAME , SUCCESSFUL_LOGIN  , WRONG_PASSWORD
 }
--- src/controllers/graphical/MainMenuController.java
+++ src/controllers/graphical/MainMenuController.java
@@ -1,11 +1,16 @@
 package controllers.graphical;
 
+import controllers.console.AccountMenu;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.effect.Bloom;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import runners.Main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -29,12 +34,15 @@ public class MainMenuController implements Initializable {
     }
 
     public void goToShop() {
+
     }
 
     public void goToHistoryMenu() {
     }
 
-    public void logOut() {
-
+    public void logOut() throws IOException {
+        AccountMenu.setLoginAccount(null);
+        Parent root =  FXMLLoader.load(getClass().getResource("../../layouts/accountPage.fxml"));
+        Main.getStage().getScene().setRoot(root);
     }
 }
