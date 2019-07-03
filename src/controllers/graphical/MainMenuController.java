27b449f0f749667ff658af722aaff2caa67af6b9
mhgheisarieh <44923177+mhgheisarieh@users.noreply.github.com>
2019-07-03 19:34:51 +0430

    global chat form created

--- src/controllers/graphical/MainMenuController.java
+++ src/controllers/graphical/MainMenuController.java
@@ -5,42 +5,21 @@ import com.google.gson.GsonBuilder;
 import controllers.console.AccountMenu;
 import controllers.console.BattleMenu;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import layouts.AlertHelper;
 import models.Account;
 import models.Shop;
-import models.cards.hero.Hero;
-import models.cards.minion.Minion;
-import models.cards.spell.Spell;
-import models.item.Item;
 import network.Client;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Formatter;
-import java.util.ResourceBundle;
-import java.util.Scanner;
 
-public class MainMenuController {
-    public Label customCardLabel;
-    public Label playLabel;
-    public Label collectionLabel;
-    public ImageView playBullet;
-    public ImageView collectionPlay;
-    public ImageView customCard;
-    public ImageView exitImage;
-    public Label lbl_exit;
-    public ImageView saveAccount;
-    public Label saveAccountLabel;
+public class MainMenuController { ;
     public Account loginAccount = AccountMenu.getLoginAccount();
 
     public static GsonBuilder gsonBuilder;
@@ -73,15 +52,15 @@ public class MainMenuController {
         Client.getStage().getScene().setRoot(AccountMenu.getRoot());
     }
 
-    public void goToCustomCardMenu(MouseEvent mouseEvent) throws IOException {
+    public void goToCustomCardMenu() throws IOException {
         Client.getStage().getScene().setRoot(FXMLLoader.load(getClass().getResource("../../layouts/customCardCreatePage.fxml")));
     }
 
-    public void exit(MouseEvent mouseEvent) {
+    public void exit() {
         System.exit(0);
     }
 
-    public void saveAccount(MouseEvent mouseEvent) {
+    public void saveAccount() {
         if(loginAccount == null)
             return;
         gsonBuilder = new GsonBuilder();
@@ -98,5 +77,9 @@ public class MainMenuController {
             e.printStackTrace();
         }
         AlertHelper.showAlert(Alert.AlertType.INFORMATION, Client.getStage().getOwner(), "Account Saved!", "Account " + loginAccount.getUserName() + " Saved!");
+    }
+
+    public void goToGlobalChatForm() throws IOException {
+        Client.getStage().getScene().setRoot(FXMLLoader.load(getClass().getResource("../../layouts/globalChatForm.fxml")));
     }
 }
